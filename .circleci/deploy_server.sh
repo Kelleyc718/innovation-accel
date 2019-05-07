c05e02db349f68d1bd7048e3aac42b0878c97485
Jason <jasosmc@yahoo.com>
2019-05-07 00:40:31 -0500

    Add deployment config files

---
+++ .circleci/deploy_server.sh
@@ -0,0 +1,5 @@
+sudo rm -rf /home/ubuntu
+cd /home/ubuntu
+git clone https://github.com/Kelleyc718/innovation-accel.git
+cd innovation-accel
+docker-compose up
---
+++ .circleci/disable_host_key_checking.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Disable host checking #
+if [ ! -d $HOME/.ssh ]
+then
+    mkdir -p $HOME/.ssh
+fi
+
+touch $HOME/.ssh/config
+echo "StrictHostKeyChecking no" >> $HOME/.ssh/config
