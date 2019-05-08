1ff155343073d643426c69d862b9d5db7b0534a0
Jason <jasosmc@yahoo.com>
2019-05-08 01:43:13 -0500

    Adjust docker-compose up in server deploy script

--- .circleci/deploy_server.sh
+++ .circleci/deploy_server.sh
@@ -4,4 +4,4 @@ cd ..
 sudo rm -rf /home/ubuntu/app/innovation-accel
 git clone https://github.com/Kelleyc718/innovation-accel.git
 cd innovation-accel
-sudo docker-compose up
+sudo docker-compose up -d
