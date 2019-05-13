35c33c45b1e42bb1c0151357e9f6d80ddd141837
Salman Rana <salman.r.rana@gmail.com>
2019-05-13 13:17:43 -0500

    sensor and visualizations components wireframe

---
+++ client/src/components/Sensors.js
@@ -0,0 +1,22 @@
+import "../css/Sensors.css";
+import React, { useEffect } from "react";
+// import axios from "axios";
+
+// This is the type of call we would make to the backend
+// to get the sensor data according to chris K
+// useEffect(() => {
+//   axios
+//     .get(`http://${window.location.host}:5000/refineries`)
+//     .then(data => {
+//       console.log(data);
+//     })
+//     .catch(err => {
+//       console.log(err);
+//     });
+// }, []);
+
+const Sensors = props => {
+  return <div className="pageLayout">Sensors</div>;
+};
+
+export default Sensors;
