1338cdc6970ed15a22a8fe46c114deff13883089
Salman Rana <salman.r.rana@gmail.com>
2019-06-05 23:30:23 -0500

    sensor page update

--- client/src/components/Sensors.js
+++ client/src/components/Sensors.js
@@ -1,98 +1,14 @@
-// import "../css/Sensors.css";
-// import React, { useEffect } from "react";
-// // import axios from "axios";
-//
-// // This is the type of call we would make to the backend
-// // to get the sensor data according to chris K
-// // useEffect(() => {
-// //   axios
-// //     .get(`http://${window.location.host}:5000/refineries`)
-// //     .then(data => {
-// //       console.log(data);
-// //     })
-// //     .catch(err => {
-// //       console.log(err);
-// //     });
-// // }, []);
-//
-// const Sensors = props => {
-//   return <div className="pageLayout">Sensors</div>;
-// };
-//
-// export default Sensors;
-
-// import "../css/Sensors.css";
-// import React from "react";
-// import { Button } from "semantic-ui-react";
-// // import axios from "axios";
-//
-// class Sensors extends React.Component {
-//   state = {
-//     currentFrame: "frame2"
-//   };
-//
-//   chooseFrame = () => {
-//     if (this.state.currentFrame === "frame1") {
-//       return (
-//         <iframe
-//           className="sensorModelFrame"
-//           src="http://motionless-day.surge.sh"
-//           title="Landing Frame"
-//         />
-//       );
-//     }
-//
-//     if (this.state.currentFrame === "frame2") {
-//       return (
-//         <iframe
-//           className="sensorModelFrame"
-//           src="http://vacuous-pull.surge.sh/"
-//           title="Landing Frame"
-//         />
-//       );
-//     }
-//   };
-//
-//   onButtonClick = () => {
-//     this.setState({
-//       currentFrame: "frame1"
-//     });
-//   };
-//   onOtherButtonClick = () => {
-//     this.setState({
-//       currentFrame: "frame2"
-//     });
-//   };
-//
-//   render() {
-//     return (
-//       <div className="sensorsPageLayout">
-//         <Button primary onClick={this.onButtonClick}>
-//           Reinfery1
-//         </Button>
-//         <Button primary onClick={this.onOtherButtonClick}>
-//           Refinery2
-//         </Button>
-//
-//         {this.chooseFrame()}
-//       </div>
-//     );
-//   }
-// }
-//
-// export default Sensors;
-
 import "../css/Sensors.css";
 import React from "react";
-import { Button } from "semantic-ui-react";
-// import axios from "axios";
+import SensorNav from "./SensorsHelpers/SensorNav";
+import SensorButtonArea from "./SensorsHelpers/SensorButtonArea";
 
 const API_SENSOR_URL = "http://localhost:5000/sensordata";
 
 class Sensors extends React.Component {
   state = {
-    currentFrame: "frame1",
-    sensorData: []
+    sensorData: [],
+    currentModel: "http://127.0.0.1:9000/allTheTests/"
   };
 
   componentDidMount() {
@@ -116,83 +32,60 @@ class Sensors extends React.Component {
     });
   };
 
-  chooseFrame = () => {
-    if (this.state.currentFrame === "frame1") {
-      return (
-        <iframe
-          className="sensorModelFrame"
-          src="http://flat-banana.surge.sh/"
-          title="Landing Frame"
-        />
-      );
-    }
-
-    if (this.state.currentFrame === "frame2") {
-      return (
-        <iframe
-          className="sensorModelFrame"
-          src="http://vacuous-pull.surge.sh/"
-          title="Landing Frame"
-        />
-      );
-    }
+  onPumpClick = e => {
+    e.preventDefault();
+    setTimeout(() => {
+      this.setState({
+        currentModel: "http://127.0.0.1:9000/Pump_ProjectMay/"
+      });
+    }, 0);
   };
 
-  onButtonClick = () => {
-    this.setState({
-      currentFrame: "frame1"
-    });
+  onBoilerClick = e => {
+    e.preventDefault();
+    setTimeout(() => {
+      this.setState({
+        currentModel: "http://127.0.0.1:9000/Boiler_ProjectMay/"
+      });
+    }, 0);
   };
-  onOtherButtonClick = () => {
-    this.setState({
-      currentFrame: "frame2"
-    });
+
+  onGeneratorClick = e => {
+    e.preventDefault();
+    setTimeout(() => {
+      this.setState({
+        currentModel: "http://127.0.0.1:9000/Generator_MayProject/"
+      });
+    }, 0);
+  };
+
+  onDesalterClick = e => {
+    e.preventDefault();
+    setTimeout(() => {
+      this.setState({
+        currentModel: "http://127.0.0.1:9000/Desalter_MayProject/"
+      });
+    }, 0);
   };
 
   render() {
     console.log("the state is: ", this.state.sensorData);
     return (
       <div className="sensorsPageLayout">
+        <SensorNav />
         <div className="sensorButtonArea">
-          <Button
-            className="singleButton"
-            basic
-            size="huge"
-            inverted
-            onClick={this.onButtonClick}
-          >
-            Boiler
-          </Button>
-          <Button
-            className="singleButton"
-            basic
-            size="huge"
-            inverted
-            onClick={this.onOtherButtonClick}
-          >
-            Pump
-          </Button>
-          <Button
-            className="singleButton"
-            basic
-            size="huge"
-            inverted
-            onClick={this.onOtherButtonClick}
-          >
-            Filter
-          </Button>
-          <Button
-            className="singleButton"
-            basic
-            size="huge"
-            inverted
-            onClick={this.onOtherButtonClick}
-          >
-            Generator
-          </Button>
+          <SensorButtonArea
+            onBoilerClick={this.onBoilerClick}
+            onPumpClick={this.onPumpClick}
+            onGeneratorClick={this.onGeneratorClick}
+            onDesalterClick={this.onDesalterClick}
+          />
         </div>
-
-        {this.chooseFrame()}
+        <iframe
+          className="sensorModelFrame"
+          src={this.state.currentModel}
+          title="Landing Frame"
+        />
       </div>
     );
   }
